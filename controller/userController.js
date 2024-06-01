b40b683b72a208a6bf47b0b9f6b8651305ebf54a
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2024-06-01 09:22:55 +0000

    added resolvers/controllers for user schema

---
+++ controller/userController.js
@@ -0,0 +1,28 @@
+// resolver methods for grapql queries , mutations 
+const User  =  require('../model/userModel')
+ 
+const getUser = async ( { id } ) => {
+    const user  = await User.finfById(id);
+    return user;
+}
+
+const getUsers = async () => {
+    const users  = await User.find();
+    return users;
+}
+
+const createUser = async ({ name , email , password }) {
+    const user  =  new User ({ name , emai , password });
+    await user.save();
+    return user;
+}
+
+const updateUser  = async ({ id , name , email , password }) => {
+    const user  = await User.findByIdAndUpdate(id, { name , email , password});
+    return user;
+}
+
+const deleteUser  = async ({ id }) => {
+    const user  =  await User.findByIdAndDelete(id);
+    return user;
+}
