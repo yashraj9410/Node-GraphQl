f1c20dd4e2ef9df0dddc029a997e8da4304929c5
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2024-06-01 06:46:52 +0000

    added user graphql schema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,3 +1,21 @@
 const { buildSchema } = require('graphql')
 
-const 
+module.exports =  buildSchema(`
+    type User {
+        id: ID!,
+        name: String!,
+        email: String!,
+        password: String!
+    }
+
+    type Query {
+        getUser( id : ID! ) : User,
+        getUsers : [User]
+    }
+
+    type Mutation {
+        createUser(name : String! , email : String! , password : String!) : User,
+        updateUser(id : ID! ,  name: String! ,  email:String! , password:String!) : User,
+        deleteUser(id : ID!) : User 
+    }
+`);
